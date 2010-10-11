3a123e07737ec831a1f6dcf1b64326bd0003aca7
unknown <Rodrigo@.(none)>
2010-10-10 23:12:42 -0600

    Fixing a problem on the create users migration.

--- db/migrate/20101010000956_create_users.rb
+++ db/migrate/20101010000956_create_users.rb
@@ -4,12 +4,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :login
       t.string :crypted_password
       t.string :salt
-      t.varchar :name
+      t.string :name
       t.string :last_name
       t.string :email
       t.string :persistence_token
       t.string :current_login_ip
-      t.varchar :last_login_ip
+      t.string :last_login_ip
       t.datetime :current_login_at
       t.datetime :last_request_at
       t.integer :failed_login_count
