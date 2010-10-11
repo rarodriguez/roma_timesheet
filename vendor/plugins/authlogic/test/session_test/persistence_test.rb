8c7405166254105eb73a2941634f9c7fc8b7b242
unknown <Rodrigo@.(none)>
2010-10-10 22:57:27 -0600

    Adding Authlogic

---
+++ vendor/plugins/authlogic/test/session_test/persistence_test.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../test_helper.rb'
+
+module SessionTest
+  class PersistenceTest < ActiveSupport::TestCase
+    def test_find
+      ben = users(:ben)
+      assert !UserSession.find
+      http_basic_auth_for(ben) { assert UserSession.find }
+      set_cookie_for(ben)
+      assert UserSession.find
+      unset_cookie
+      set_session_for(ben)
+      session = UserSession.find
+      assert session
+    end
+    
+    def test_persisting
+      # tested thoroughly in test_find
+    end
+    
+    def test_should_set_remember_me_on_the_next_request
+      ben = users(:ben)
+      session = UserSession.new(ben)
+      session.remember_me = true
+      assert UserSession.remember_me == false
+      assert session.save
+      assert session.remember_me?
+      session = UserSession.find(ben)
+      assert session.remember_me?
+    end
+  end
+end
---
+++ vendor/plugins/authlogic/test/session_test/timeout_test.rb
@@ -0,0 +1,81 @@
+require File.dirname(__FILE__) + '/../test_helper.rb'
+
+module SessionTest
+  module TimeoutTest
+    class ConfigTest < ActiveSupport::TestCase
+      def test_logout_on_timeout
+        UserSession.logout_on_timeout = true
+        assert UserSession.logout_on_timeout
+    
+        UserSession.logout_on_timeout false
+        assert !UserSession.logout_on_timeout
+      end
+    end
+    
+    class InstanceMethods < ActiveSupport::TestCase
+      def test_stale_state
+        UserSession.logout_on_timeout = true
+        ben = users(:ben)
+        ben.last_request_at = 3.years.ago
+        ben.save
+        set_session_for(ben)
+      
+        session = UserSession.new
+        assert session.persisting?
+        assert session.stale?
+        assert_equal ben, session.stale_record
+        assert_nil session.record
+        assert_nil controller.session["user_credentials_id"]
+      
+        set_session_for(ben)
+      
+        ben.last_request_at = Time.now
+        ben.save
+      
+        assert session.persisting?
+        assert !session.stale?
+        assert_nil session.stale_record
+      
+        UserSession.logout_on_timeout = false
+      end
+      
+      def test_should_be_stale_with_expired_remember_date
+        UserSession.logout_on_timeout = true
+        UserSession.remember_me = true
+        UserSession.remember_me_for = 3.month
+        ben = users(:ben)
+        ben.save
+        session = UserSession.new(ben)
+        session.save
+        Timecop.freeze(Time.now + 4.month)
+        assert session.persisting?
+        assert session.stale?
+        UserSession.remember_me = false
+      end
+
+      def test_should_not_be_stale_with_valid_remember_date
+        UserSession.logout_on_timeout = true  #10.minutes
+        UserSession.remember_me = true
+        UserSession.remember_me_for = 3.month
+        ben = users(:ben)
+        ben.save
+        session = UserSession.new(ben)
+        session.save
+        Timecop.freeze(Time.now + 2.month)
+        assert session.persisting?
+        assert !session.stale?
+        UserSession.remember_me = false
+      end
+      
+      
+      def test_successful_login
+        UserSession.logout_on_timeout = true
+        ben = users(:ben)
+        assert UserSession.create(:login => ben.login, :password => "benrocks")
+        assert session = UserSession.find
+        assert_equal ben, session.record
+        UserSession.logout_on_timeout = false
+      end
+    end
+  end
+end
