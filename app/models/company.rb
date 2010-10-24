74f30a5460d1154ddd0f3bbb2c84144bc31ff54a
Gamasaro <rorodr@gmail.com>
2010-10-23 23:31:39 -0600

    Uploading new design of almost all the basic pages. Pending design for the pages that we should create from the begining.

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,8 @@ class Company < ActiveRecord::Base
   belongs_to :manager, :class_name => 'user'
   has_many :projects
   has_and_belongs_to_many :users
+  
+  validates_presence_of :name
+  validates_presence_of :description
+  
 end
