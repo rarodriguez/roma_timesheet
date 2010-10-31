24207d0bcfe52f17934d11fec301f3b735e8e5c9
Gamasaro <rorodr@gmail.com>
2010-10-31 14:48:53 -0600

    Uploading a fix for company relations. Also uploading the first editable grid (using jqgrid external form).

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
-  belongs_to :manager, :class_name => "User"
+  belongs_to :manager, :class_name => "User", :foreign_key => 'user_id'
   has_many :projects
   has_and_belongs_to_many :users
   has_many :timecards, :through => :projects
